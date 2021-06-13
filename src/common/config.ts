1f241f00bf4bf5b32c701f39bdeadba1d8616ea8
Aliaksandr Tarhonski <stdmnstar@gmail.com>
2021-06-13 17:29:32 +0300

    refactor: cnange sripts in package.json

--- src/common/config.ts
+++ src/common/config.ts
@@ -9,6 +9,10 @@ export default {
   PORT: process.env['PORT'],
   NODE_ENV: process.env['NODE_ENV'],
   MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
+  POSTGRES_PORT: process.env['POSTGRES_PORT'],
+  POSTGRES_USER: process.env['POSTGRES_USER'],
+  POSTGRES_PASSWORD: process.env['POSTGRES_PASSWORD'],
+  POSTGRES_DB: process.env['POSTGRES_DB'],
   JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
   AUTH_MODE: process.env['AUTH_MODE'] === 'true',
 };
